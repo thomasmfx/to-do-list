e30f8667c3640aa4566a2333ca46e13bfc7dbc39
thomasmfx <thomasmoisesf@gmail.com>
2024-06-23 16:14:34 -0300

    feat: Add favicon via import

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import favicon from '../assets/favicon.svg';
+
+function setFavicons(favicon){
+    let headTitle = document.querySelector('head');
+    let setFavicon = document.createElement('link');
+    setFavicon.setAttribute('rel', 'shortcut icon');
+    setFavicon.setAttribute('href', favicon);
+    headTitle.appendChild(setFavicon);
+};
+setFavicons(favicon);
