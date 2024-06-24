a903273ce455c4895ec0cee177acd73e7601cad9
thomasmfx <thomasmoisesf@gmail.com>
2024-06-24 11:44:12 -0300

    Add logo in header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
 import './style.css';
-import favicon from '../assets/favicon.svg';
-
-function setFavicons(favicon){
-    let headTitle = document.querySelector('head');
-    let setFavicon = document.createElement('link');
-    setFavicon.setAttribute('rel', 'shortcut icon');
-    setFavicon.setAttribute('href', favicon);
-    headTitle.appendChild(setFavicon);
-};
-setFavicons(favicon);
+import setImages from './logo.js';
