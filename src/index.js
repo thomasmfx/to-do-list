28624106b80231406b0a4aaaaa8e6422f9673f69
thomasmfx <thomasmoisesf@gmail.com>
2024-06-24 17:26:38 -0300

    feat: Add function to display task cards in DOM by tasks array

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import './style.css';
 import setImages from './modules/logo.js';
-import Task from './modules/newTask.js';
-import Project from './modules/newProject.js';
+import { Task, newTaskCard } from './modules/newTask.js';
+// import Project from './modules/newProject.js';
 
 let tasks = [];
-let projects = [];
+// let projects = [];
 
 
-// const header = new Task('Add header', 'To-do List', '24-06-2024', 'High', 'Header components: Logo, dark/light mode theme icon, and login button');
-// tasks.push(header);
-// console.table(tasks);
+const header = new Task('Add header', 'To-do List', '24-06-2024', 'high', 'Header components: Logo, dark/light mode theme icon, and login button');
+tasks.push(header);
+
+function displayTasks(arr) {
+    for(const task of arr) {
+        newTaskCard(task)
+    };
+};
+
+displayTasks(tasks)
 
 // const todo = new Project('To-do List', 'sla.com', 'To-do List from The Odin Project');
 // projects.push(todo);
--- src/modules/newTask.js
+++ src/modules/newTask.js
@@ -1,3 +1,5 @@
+import create from './domCreator.js';
+
 class Task {
     constructor(title, project, dueDate, priority, description) {
         this.title = title,
@@ -10,4 +12,46 @@ class Task {
 
 };
 
-export default Task;
+function newTaskCard(task) {
+    const minView = document.querySelector('#min-view');
+    // Card
+    // | Priority
+    // | Wrapper
+    // | | Checkbox
+    // | | Info
+    // | | | Title
+    // | | | Project
+    // | | Date and Actions
+    // | | | Due date
+    // | | | Edit task btn
+    // | | | Remove task button
+    const card = create.elWithClass('div', 'task-card');
+
+    const priority = create.elWithClass('div', 'priority');
+    priority.classList.add(`${task.priority}`)
+    const wrapper = create.elWithClass('div', 'wrapper');
+
+    const checkbox = create.elWithClass('input', 'check');
+    checkbox.type = 'checkbox';
+    const info = create.elWithClass('div', 'info');
+
+    const title = create.elWithClass('p', 'title', task.title);
+    const taskProject = create.elWithClass('p', 'project', task.project);
+
+    const dateAndActions = create.elWithClass('div', 'date-and-actions');
+
+    const dueDate = create.elWithClass('p', 'dueDate', task.dueDate);
+    const editBtn = create.el('button');
+    const removeBtn = create.el('button');
+    editBtn.classList.add('fa-regular', 'fa-pen-to-square', 'edit');
+    removeBtn.classList.add('fa-regular', 'fa-square-minus', 'remove');
+
+    dateAndActions.append(dueDate, editBtn, removeBtn);
+    info.append(title, taskProject);
+    wrapper.append(checkbox, info, dateAndActions);
+    card.append(priority, wrapper);
+
+    minView.appendChild(card)
+};
+
+export { Task, newTaskCard }
