94ba6d88380b67112a891d85255fbaf5e94fcb2b
thomasmfx <thomasmoisesf@gmail.com>
2024-06-24 11:43:08 -0300

    refactor: Change file name and update functions due to 'domCreate' changes

---
+++ src/logo.js
@@ -0,0 +1,24 @@
+import favicon from '../assets/favicon.svg';
+import headerLogo from '../assets/logo.svg';
+import create from './domCreate.js';
+
+export default function setImages(){
+    // Favicon
+    let headTitle = document.querySelector('head');
+    let setFavicon = create.el('link');
+    setFavicon.setAttribute('rel', 'shortcut icon');
+    setFavicon.setAttribute('href', favicon);
+
+    headTitle.appendChild(setFavicon);
+
+    // Header logo
+    let logoDiv = document.querySelector('#logo')
+    let logo = create.imgWithSrc(headerLogo);
+    let logoText = create.el('p', 'Due');
+    logoText.style.color = 'var(--french-gray)'
+
+    logoDiv.appendChild(logo)
+    logoDiv.appendChild(logoText);
+};
+
+setImages();
