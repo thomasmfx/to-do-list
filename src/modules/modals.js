2a0bf157eeb6abf8581ef5a942922c1c006c4347
thomasmfx <thomasmoisesf@gmail.com>
2024-06-26 20:03:04 -0300

    feat: Start to implement task submit

--- src/modules/modals.js
+++ src/modules/modals.js
@@ -1,12 +1,27 @@
-const optionsModal = document.querySelector('#modal-options')
-const formsModal = document.querySelector('#modal-forms');
+import { clearCards } from "../index.js";
+import { Task, pushTask, displayTasks } from "./taskControls.js";
+
 const createBtn = document.querySelector('#create');
+
+const optionsModal = document.querySelector('#modal-options');
 const createTaskBtn = document.querySelector('#create-task');
 const createProjBtn = document.querySelector('#create-project');
+
+const formsModal = document.querySelector('#modal-forms');
 const taskForm = document.querySelector('#task-form');
+const addTask = document.querySelector('#add-task');
 const projForm = document.querySelector('#project-form');
+const addProj = document.querySelector('#add-project');
 
 function modals(){
+    addTask.addEventListener('click', function(event) {
+        event.preventDefault();
+    });
+
+    addProj.addEventListener('click', function(event) {
+        event.preventDefault();
+    });
+    
     createBtn.addEventListener('click', () => {
         optionsModal.style.visibility = 'visible';
         closeModalBheavior(optionsModal);
@@ -38,5 +53,16 @@ function closeModalBheavior(modal) {
         };
     };
 };
+
+// function submitTask() {
+//     const title = document.querySelector('#task-title').value;
+//     const project = document.querySelector('#projects-dropdown').value;
+//     const priority = document.querySelector('#task-priority').value;
+//     const dueDate = document.querySelector('#due-date').value;
+//     const description = document.querySelector('#project-desrcription').value;
+
+//     let task = new Task(title, project)
+// };
+
 
 export default modals;
