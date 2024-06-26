3a0d4de1a2213026b57e44b9287c7e003d81f3be
thomasmfx <thomasmoisesf@gmail.com>
2024-06-26 14:06:56 -0300

    feat: Add 'New project' form to modal

--- src/modules/modals.js
+++ src/modules/modals.js
@@ -3,6 +3,8 @@ const formsModal = document.querySelector('#modal-forms');
 const createBtn = document.querySelector('#create');
 const createTaskBtn = document.querySelector('#create-task');
 const createProjBtn = document.querySelector('#create-project');
+const taskForm = document.querySelector('#task-form');
+const projForm = document.querySelector('#project-form');
 
 function modals(){
     createBtn.addEventListener('click', () => {
@@ -11,14 +13,16 @@ function modals(){
     });
     
     createTaskBtn.addEventListener('click', () => {
-        optionsModal.style.visibility = "hidden";
+        optionsModal.style.visibility = 'hidden';
         formsModal.style.visibility = 'visible';
+        taskForm.style.visibility = 'visible';
         closeModalBheavior(formsModal);
     });
     
     createProjBtn.addEventListener('click', () => {
-        optionsModal.style.visibility = "hidden";
+        optionsModal.style.visibility = 'hidden';
         formsModal.style.visibility = 'visible';
+        projForm.style.visibility = 'visible';
         closeModalBheavior(formsModal);
     });
 };
@@ -26,7 +30,9 @@ function modals(){
 function closeModalBheavior(modal) {
     window.onclick = function(event) {
         if (event.target === modal) {
-            modal.style.visibility = "hidden";
+            modal.style.visibility = 'hidden';
+            taskForm.style.visibility = 'hidden';
+            projForm.style.visibility = 'hidden';
         };
     };
 };
